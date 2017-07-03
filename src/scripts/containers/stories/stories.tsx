0a0833a15811924aff01fd25105c83416e2871bc
Anurag Choudhary <anurag.choudhary93@gmail.com>
2017-07-02 23:12:39 -0700

    added stories, blogs, bump bundle

--- src/scripts/containers/stories/stories.tsx
+++ src/scripts/containers/stories/stories.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 export const Stories = () => {
   // document.body.style.backgroundColor = "green";
+  const mockStories = [
+    {}, {}, {}, {}, {}, {}, {}, {}, {}
+  ]
+
+  const storyGrid = mockStories.map((story, index) => {
+    return (
+      <div key={index} className="tip-story-box" />
+    )
+  })
   return (
-    <div className="tip-tiny-text">
-      What are stories really?<br/>
-      WRITTEN BY BLAHMABA LLAMA.
+    <div className="row tip-stories">
+      <div className="tip-story-grid">
+        {storyGrid}
+      </div>
     </div>
   );
 }
