4d46a5619d3270923fe44b2aad6bb1f165befa67
Anurag Choudhary <anurag.choudhary93@gmail.com>
2017-07-03 02:44:13 -0700

    fixed header margin and add placeholder content

--- src/scripts/containers/home/home.tsx
+++ src/scripts/containers/home/home.tsx
@@ -12,10 +12,13 @@ export const Home = () =>
             </div>
           </div>
           <div className="small-12 large-6 columns tip-subsection">
-            <div className="tip-massive-heading">
-              That would be kind of awesome, right?
+            <div className="tip-text">
+              In teacher education programmes – and in continuing professional development – a lot of time is devoted to the ‘what’ of teaching – what areas we should we cover, what resources do we need and so on. The ‘how’ of teaching also gets a great deal of space – how to structure a lesson, manage classes, assess for learning for learning and so on. Sometimes, as Parker J. Palmer (1998: 4) comments, we may even ask the “why” question – ‘for what purposes and to what ends do we teach? ‘But seldom, if ever’, he continues.
             </div>
           </div>
+          <div className="small-12 columns tip-subsection">
+            <img style={{width:"100%", height:"auto"}}src="http://i.dailymail.co.uk/i/pix/2017/03/28/12/3EB4345B00000578-4356398-image-a-52_1490700463455.jpg"/>
+            </div>
         </div>
       </div>
     </div>
