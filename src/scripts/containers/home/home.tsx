0156a8961355d6e1072b17e53a3fb141ab37031c
Anurag Choudhary <anurag.choudhary93@gmail.com>
2017-08-03 23:17:18 -0700

    restyle home

--- src/scripts/containers/home/home.tsx
+++ src/scripts/containers/home/home.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BbAnimatedText} from "../../building-blocks/bb-animated-text";
+import { BbAnimatedText } from "../../building-blocks/bb-animated-text";
 
 export const Home = () => <div>
   <div className="row row-center">
@@ -7,22 +7,20 @@ export const Home = () => <div>
       <div className="row tip-home-section">
         <div className="small-12 columns tip-home-subsection">
           <img
-            style={{
-            width: "100%",
-            height: "auto"
-          }}
-            src="http://technorhetoric.net/20.2/reviews/almjeld-et-al/media/images/feministreview.png"/>
+            src="http://technorhetoric.net/20.2/reviews/almjeld-et-al/media/images/feministreview.png" />
         </div>
+      </div>
+      <div className="row tip-home-section">
 
-        <div className="row tip-home-section">
-          <div className="small-12 large-6 columns tip-home-subsection">
-            <div className="tip-home-massive-heading">
-              We're breaking gender stereotypes in the everyday classroom.
+        <div className="small-12 large-6 columns tip-home-subsection">
+          <div className="tip-home-massive-heading">
+            {/* <BbAnimatedText strings={["We're breaking gender stereotypes in the everyday classroom.", "We tell children stories."] } infinite={true} iterations={10} characterTimeout={5} /> */}
+            We're breaking gender stereotypes in the everyday classroom.
             </div>
-          </div>
-          <div className="small-12 large-6 columns tip-home-subsection">
-            <p>
-              Stereotypical beliefs about gender, which rigidly characterize differences
+        </div>
+        <div className="small-12 large-6 columns tip-home-subsection">
+          <p>
+            Stereotypical beliefs about gender, which rigidly characterize differences
               between sexes and appropriate gender roles, are pervasive in most societies.
               These deep-seated beliefs reflect the society’s expectations that individuals
               behave and make choices in accordance with their gender. Examples of gender
@@ -31,16 +29,16 @@ export const Home = () => <div>
               their children or steering them (subtly or blatantly) toward certain educational
               and occupational choices that are deemed to be suitable for their gender.
             </p>
-          </div>
         </div>
+      </div>
 
-        <div className="row tip-home-section">
-          <div className="small-12 large-6 columns tip-home-subsection">
-            <div className="tip-home-subsection-heading">
-              Why is this a real problem?
+      <div className="row tip-home-section row-wrap-reverse">
+        <div className="small-12 large-6 columns tip-home-subsection">
+          <div className="tip-home-subsection-heading">
+            Why is this a real problem?
             </div>
-            <p>
-              Through the inter-generational transmission of preferences and beliefs, families
+          <p>
+            Through the inter-generational transmission of preferences and beliefs, families
               have the earliest, most direct impact on the development of children’s cognitive
               and social development. The channels of such transmission could be outright
               didactic teaching (indoctrination) or a more indirect influence via parenting
@@ -51,32 +49,32 @@ export const Home = () => <div>
               chores are females’ responsibility and males have more say in important
               decisions.
             </p>
-          </div>
-          <div className="small-12 large-6 columns tip-home-subsection">
-            <img
-              style={{
-              width: "100%",
-              height: "auto"
-            }}
-              src="http://bloximages.chicago2.vip.townnews.com/dailyuw.com/content/tncms/assets/v3/editorial/d/06/d06b2394-839b-11e6-9543-1f393d66c391/57e89a6749ea5.image.jpg?resize=1200%2C944"/>
-          </div>
         </div>
-
-        <div className="row tip-home-section row-wrap-reverse">
-          <div className="small-12 large-6 columns tip-home-subsection">
-            <img
-              style={{
+        <div className="small-12 large-6 columns tip-home-subsection">
+          <img
+            style={{
               width: "100%",
               height: "auto"
             }}
-              src="http://1.darkroom.stylist.co.uk/980/0631dc4d2a1386d0514f81b330ec61de:e44d18f472fcbb44095d9e17ce1a1ffd/simpsons-sex-ed.png"/>
-          </div>
-          <div className="small-12 large-6 columns tip-home-subsection">
-            <div className="tip-home-subsection-heading">
-              How are we solving it?
+            src="http://bloximages.chicago2.vip.townnews.com/dailyuw.com/content/tncms/assets/v3/editorial/d/06/d06b2394-839b-11e6-9543-1f393d66c391/57e89a6749ea5.image.jpg?resize=1200%2C944" />
+        </div>
+      </div>
+
+      <div className="row tip-home-section">
+        <div className="small-12 large-6 columns tip-home-subsection">
+          <img
+            style={{
+              width: "100%",
+              height: "auto"
+            }}
+            src="http://1.darkroom.stylist.co.uk/980/0631dc4d2a1386d0514f81b330ec61de:e44d18f472fcbb44095d9e17ce1a1ffd/simpsons-sex-ed.png" />
+        </div>
+        <div className="small-12 large-6 columns tip-home-subsection">
+          <div className="tip-home-subsection-heading">
+            How are we solving it?
             </div>
-            <p>
-              Use stories to retell the narrative. Stories are illustrated and set in the
+          <p>
+            Use stories to retell the narrative. Stories are illustrated and set in the
               Indian setting ( so you won’t find a robert or a nora in the stories). Stories
               have characters who are brown skinned, hairy, wear gender neutral clothing, make
               choices, learn about consent etc. Illustrations were damn important for our work
@@ -86,9 +84,7 @@ export const Home = () => <div>
               same vein, developing their emotional literacy. We cover topics like
               commonalities, uniqueness, acceptance, emotional literacy.
             </p>
-          </div>
         </div>
-
       </div>
     </div>
   </div>
