d1a54e27106d13d142d39038bdc39d7a1b59a3b3
Anurag Choudhary <anurag.choudhary93@gmail.com>
2017-08-03 23:18:30 -0700

    update base styles

--- src/scripts/containers/main/footer/footer.tsx
+++ src/scripts/containers/main/footer/footer.tsx
@@ -3,5 +3,61 @@ import { Switch, Route, Link, NavLink } from 'react-router-dom';
 
 export const Footer = () => (
     <div className="tip-footer">
+
+        {/* <div className="tip-footer-section">
+            <h3 className="tip-footer-section-title">STORIES</h3>
+            <h4 className="tip-footer-section-subtitle">
+                All Stories
+            </h4>
+                        <h4 className="tip-footer-section-subtitle">
+                Latest Story
+            </h4>
+                        <h4 className="tip-footer-section-subtitle">
+                First Story
+            </h4>
+        </div>
+        <div className="tip-footer-section">
+            <h3 className="tip-footer-section-title">BLOG</h3>
+            <h4 className="tip-footer-section-subtitle">
+                All Blogs
+            </h4>
+                        <h4 className="tip-footer-section-subtitle">
+                Latest Blog
+            </h4>
+                        <h4 className="tip-footer-section-subtitle">
+                Write a Blog
+            </h4>
+        </div>
+                <h1 className="tip-title anim-text-flow">
+            <span>t</span>
+            <span>h</span>
+            <span>e</span>
+            <span />
+            <span />
+            <span />
+            <span />
+            <span>i</span>
+            <span>r</span>
+            <span>r</span>
+            <span>e</span>
+            <span>l</span>
+            <span>e</span>
+            <span>v</span>
+            <span>a</span>
+            <span>n</span>
+            <span>t</span>
+            <span />
+            <span />
+            <span />
+            <span />
+            <span>p</span>
+            <span>r</span>
+            <span>o</span>
+            <span>j</span>
+            <span>e</span>
+            <span>c</span>
+            <span>t</span>
+
+        </h1> */}
     </div>
 );
