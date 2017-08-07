fb0927a3e41caad2cfc6bc192bae46702a141199
Anurag Choudhary <anurag.choudhary93@gmail.com>
2017-08-07 01:18:30 -0700

    add and style footer

--- src/scripts/containers/main/footer/footer.tsx
+++ src/scripts/containers/main/footer/footer.tsx
@@ -3,61 +3,98 @@ import { Switch, Route, Link, NavLink } from 'react-router-dom';
 
 export const Footer = () => (
     <div className="tip-footer">
+        <div className="row">
+            <div className="tip-footer-section small-6 large-3 columns">
+                <h3 className="tip-footer-section-title">STORIES</h3>
+                <h4 className="tip-footer-section-subtitle">
+                    All Stories
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Latest Story
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    First Story
+                </h4>
+            </div>
+            <div className="tip-footer-section small-6 large-3 columns">
+                <h3 className="tip-footer-section-title">
+                    BLOG
+                </h3>
+                <h4 className="tip-footer-section-subtitle">
+                    All Blogs
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Latest Blog
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Write a Blog
+                </h4>
+            </div>
+            <div className="tip-footer-section small-6 large-3 columns">
+                <h3 className="tip-footer-section-title">
+                    VOLUNTEER
+                </h3>
+                <h4 className="tip-footer-section-subtitle">
+                    Apply as a Writer
+                </h4>
 
-        {/* <div className="tip-footer-section">
-            <h3 className="tip-footer-section-title">STORIES</h3>
-            <h4 className="tip-footer-section-subtitle">
-                All Stories
-            </h4>
-                        <h4 className="tip-footer-section-subtitle">
-                Latest Story
-            </h4>
-                        <h4 className="tip-footer-section-subtitle">
-                First Story
-            </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Apply as an Illustrator
+                </h4>
+            </div>
+            <div className="tip-footer-section small-6 large-3 columns">
+                <h3 className="tip-footer-section-title">
+                    EXTERNAL
+                </h3>
+                <h4 className="tip-footer-section-subtitle">
+                    Facebook
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Twitter
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Instagram
+                </h4>
+                <h4 className="tip-footer-section-subtitle">
+                    Contact us
+                </h4>
+            </div>
         </div>
-        <div className="tip-footer-section">
-            <h3 className="tip-footer-section-title">BLOG</h3>
-            <h4 className="tip-footer-section-subtitle">
-                All Blogs
-            </h4>
-                        <h4 className="tip-footer-section-subtitle">
-                Latest Blog
-            </h4>
-                        <h4 className="tip-footer-section-subtitle">
-                Write a Blog
-            </h4>
-        </div>
-                <h1 className="tip-title anim-text-flow">
-            <span>t</span>
-            <span>h</span>
-            <span>e</span>
-            <span />
-            <span />
-            <span />
-            <span />
-            <span>i</span>
-            <span>r</span>
-            <span>r</span>
-            <span>e</span>
-            <span>l</span>
-            <span>e</span>
-            <span>v</span>
-            <span>a</span>
-            <span>n</span>
-            <span>t</span>
-            <span />
-            <span />
-            <span />
-            <span />
-            <span>p</span>
-            <span>r</span>
-            <span>o</span>
-            <span>j</span>
-            <span>e</span>
-            <span>c</span>
-            <span>t</span>
+        <div className="row">
+            <div className="tip-title">
+                <div className="tip-title-copyright">© The Irrelevant Project 2017</div>
+                <div className="tip-title-banner anim-text-flow">
+                    <span>t</span>
+                    <span>h</span>
+                    <span>e</span>
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span>i</span>
+                    <span>r</span>
+                    <span>r</span>
+                    <span>e</span>
+                    <span>l</span>
+                    <span>e</span>
+                    <span>v</span>
+                    <span>a</span>
+                    <span>n</span>
+                    <span>t</span>
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span>p</span>
+                    <span>r</span>
+                    <span>o</span>
+                    <span>j</span>
+                    <span>e</span>
+                    <span>c</span>
+                    <span>t</span>
 
-        </h1> */}
+                </div>
+            </div>
+        </div>
     </div>
 );
