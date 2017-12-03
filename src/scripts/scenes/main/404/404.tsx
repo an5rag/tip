120dbdcd7a001144b9ee98cb959ccc43b4c241ea
Anurag Choudhary <anurag.choudhary93@gmail.com>
2017-12-02 23:47:13 -0800

    style 404 page

--- src/scripts/scenes/main/404/404.tsx
+++ src/scripts/scenes/main/404/404.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as DocumentTitle from "react-document-title";
 import { Link, NavLink, Route, Switch } from "react-router-dom";
 import { TipLink } from "../../../building-blocks/tip-link";
 import { BbAnimatedColorText } from "./../../../building-blocks/bb-animated-color-text";
@@ -6,35 +7,40 @@ import { Footer } from "./../footer/footer";
 import { Header } from "./../header/header";
 
 const image = require("./static/404-hero.png");
-
-export const NotFoundPage = () => {
+export interface INotFoundProps {
+  type?: string;
+}
+export const NotFoundPage = (props: INotFoundProps) => {
+  const type = props.type ? props.type : "page";
   return (
-    <div>
-      <Header showNavBar={false} />
-      <div className="row row-center tip-404 row-wrap-reverse">
+    <DocumentTitle title={`Can't find ${type} - The Irrelevant Project`}>
+      <div>
+        {/* <Header showNavBar={false} /> */}
+        <div className="row row-center tip-404 row-wrap-reverse">
 
-        <div className="columns small-12 medium-5 columns">
+          <div className="columns small-12 medium-5 columns">
 
-          <h1>
-            Yikes.
+            <h1>
+              Yikes.
           </h1>
-          <h2>
-            We can't seem to find the page you're looking for.
+            <h2>
+              We can't seem to find the {type} you're looking for.
           </h2>
-          <p>
-            Here are some helpful links instead:<br />
-            <TipLink link="/home">Home <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
-            <TipLink link="/stories">Stories <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
-            <TipLink link="/blog">Blog <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
-            <TipLink link="/about">About <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
-            <TipLink link="/faq">Frequently Asked Questions <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink>
-          </p>
+            <p>
+              Here are some helpful links instead:<br />
+              <TipLink link="/home">Home <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
+              <TipLink link="/stories">Stories <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
+              <TipLink link="/blog">Blog <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
+              <TipLink link="/about">About <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink><br />
+              <TipLink link="/faq">Frequently Asked Questions <i className="fa fa-angle-right link-icon" aria-hidden="true"></i></TipLink>
+            </p>
+          </div>
+          <div className="columns small-12 medium-5 hero-image-container">
+            <img className="hero-image" src={image} />
+          </div>
         </div>
-        <div className="columns small-12 medium-5 hero-image-container">
-          <img className="hero-image" src={image} />
-        </div>
+        {/* <Footer /> */}
       </div>
-      <Footer />
-    </div>
+    </DocumentTitle>
   );
 };
