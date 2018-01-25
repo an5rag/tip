a757b49294dd5582050bfee6106e77f040eecade
Anurag Choudhary <anurag.choudhary93@gmail.com>
2018-01-24 20:47:39 -0800

    change target to es5, add autoprefixer, small style change for button and footer

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -85,6 +85,14 @@ var config = {
               options: {
                 sourceMap: true
               }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [
+                  require('autoprefixer')
+                ]
+              }
             }
           ],
 
